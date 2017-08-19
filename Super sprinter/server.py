c0b8b44668468281ade2f55873a68fb3c6a0a7c2
Hanga Gelencser <hangagelencser@gmail.com>
2017-08-20 00:44:38 +0200

    clean code, added delete

--- Super sprinter/server.py
+++ Super sprinter/server.py
@@ -12,18 +12,31 @@ def id_generator(chars=string.ascii_uppercase + string.ascii_lowercase + string.
 
 def read_csv():
     lst = []
-    with open("data.csv", "r") as file:
-        reader = csv.reader(file)
+    with open("data.csv", "r", newline='') as file:
+        reader = csv.reader(file, delimiter=",")
         for row in reader:
             lst.append(row)
     return lst
 
 
+def obtained_data():
+    story_title = request.args.get('title')
+    user_story = request.args.get('story')
+    acc_criteria = request.args.get('criteria')
+    business_value = request.args.get('value')
+    estimation = request.args.get('estim')
+    status = request.args.get('status')
+    user_id = id_generator()
+    actual_list = [user_id, story_title, user_story, acc_criteria, business_value, estimation, status]
+    session['user_id'] = user_id
+    return actual_list
+
+
 @app.route('/')
 def story():
-    lst = read_csv()
-    length_of_csv = len(lst)  
-    return render_template('list.html', lst=lst, long=length_of_csv)
+    list_of_data = read_csv()
+    length_of_csv = len(list_of_data)  
+    return render_template('list.html', lst=list_of_data, long=length_of_csv)
 
 
 @app.route('/new_story')
@@ -34,15 +47,7 @@ def new_story():
 @app.route('/update/<int:post_id>')
 def update_story(post_id):
     list_of_data = read_csv()
-    story_title = request.args.get('title')
-    user_story = request.args.get('story')
-    acc_criteria = request.args.get('criteria')
-    business_value = request.args.get('value')
-    estimation = request.args.get('estim')
-    status = request.args.get('status')
-    user_id = id_generator()
-    actual_list = [user_id, story_title, user_story, acc_criteria, business_value, estimation, status]
-    print (actual_list)
+    actual_list = obtained_data()
     for i in range(len(list_of_data)):
         if post_id-1 == i:
             list_of_data[i] = actual_list
@@ -55,43 +60,24 @@ def update_story(post_id):
 @app.route('/delete/<int:post_id>')
 def delete(post_id):
     list_of_data = read_csv()
-    story_title = request.args.get('title')
-    user_story = request.args.get('story')
-    acc_criteria = request.args.get('criteria')
-    business_value = request.args.get('value')
-    estimation = request.args.get('estim')
-    status = request.args.get('status')
-    user_id = id_generator()
-    actual_list = [user_id, story_title, user_story, acc_criteria, business_value, estimation, status]
-    print (list_of_data)
-    print (post_id)
     data_info = []
     for i in range(len(list_of_data)):
-        print (i)
-        print ("BENT")
         if post_id == i+1:
-            print ("IFBENT")
             continue
         else:
-            print ("ELSEBENT")
-            data_info.append(list_of_data[0])
-    print (data_info)
+            data_info.append(list_of_data[i])
+    with open("data.csv", "w", newline='') as file:
+        datawriter = csv.writer(file)
+        datawriter.writerows(data_info)
     return redirect('/')
 
-@app.route('/save', methods = ['GET'])
+
+@app.route('/save', methods=['GET'])
 def saving():
-    story_title = request.args.get('title')
-    user_story = request.args.get('story')
-    acc_criteria = request.args.get('criteria')
-    business_value = request.args.get('value')
-    estimation = request.args.get('estim')
-    status = request.args.get('status')
-    user_id = id_generator()
-    story_info = [user_id, story_title, user_story, acc_criteria, business_value, estimation, status]
-    session['user_id'] = user_id
+    story_info = obtained_data()
     with open("data.csv", "a") as file:
-        writer = csv.writer(file)
-        writer.writerow(story_info)
+        datawriter = csv.writer(file)
+        datawriter.writerow(story_info)
     return redirect('/')
 
 
@@ -99,7 +85,6 @@ def saving():
 def story_1(post_id):
     list_of_data = read_csv()
     return render_template('story_id.html', post_id=post_id, lst=list_of_data[post_id-1])
-
 
 
 if __name__ == "__main__":
